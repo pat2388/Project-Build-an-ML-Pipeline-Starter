13620a2780b276aed63e6a4b1d327b338659f0cb
pat2388 <ptiern1@wgu.edu>
2024-08-05 23:30:21 -0400

    changes to data_check and addition to main

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,10 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+# checks that the size of the dataset is reasonable (not too small, not too large)
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+# checks that the price range is between min_price and max_price.
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price)
